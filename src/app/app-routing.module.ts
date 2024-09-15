ce1b597b7a17fcabda336d0cec112d969032983b
parikshit0308 <sparikshit955@gmail.com>
2024-09-15 16:00:55 +0530

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'master', loadChildren: () => import('./master/master.module').then(m => m.MasterModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
